31cacb31ac2394b9a0823ed011d4a77d6df06e17
capstoneprojkeepney <ijunpe0@gmail.com>
2026-01-27 19:56:45 +0800

    Update data.js

--- data.js
+++ data.js
@@ -1,4 +1,4 @@
-const mapData = {
+atconst mapData = {
     "Terminals": [
         {
             "name": "Balagtas to Bulakan Terminal",
@@ -8,22 +8,22 @@ const mapData = {
             "stops": [
                 {
                     "name": "Camella, Matungao Bulacan",
-                    "fare": "₱12.00",
+                    "estimated fare": "₱12.00",
                     "coords": [120.88582555379693, 14.813388697236904]
                 },
                 {
                     "name": "Triple Junction Subdivision",
-                    "fare": "₱15.00",
+                    "estimated fare": "₱15.00",
                     "coords": [120.88451106809244, 14.812977114112307]
                 },
                 {
                     "name": "Alfamart Matungao",
-                    "fare": "₱18.00",
+                    "estimated fare": "₱18.00",
                     "coords": [120.881006295136, 14.805616011874273]
                 },
                 {
                     "name": "Diocesan Shrine and Parish of Nuestra Senora de la Asuncion (End)",
-                    "fare": "₱20.00",
+                    "estimated fare": "₱20.00",
                     "coords": [120.87939326729115, 14.795202028815105]
                 }
             ]
@@ -36,22 +36,22 @@ const mapData = {
             "stops": [
                 {
                     "name": "Waltermart Santa Maria",
-                    "fare": "₱15.00",
+                    "estimated fare": "₱15.00",
                     "coords": [120.95374006729162, 14.82275659094804]
                 },
                 {
                     "name": "PHirst Park Homes Pandi",
-                    "fare": "₱18.00",
+                    "estimated fare": "₱18.00",
                     "coords": [120.95739329612721, 14.860787255430091]
                 },
                 {
                     "name": "Immaculate Conception Parish Church Pandi",
-                    "fare": "₱21.00",
+                    "estimated fare": "₱21.00",
                     "coords": [120.95801719612722, 14.863364173221454]
                 },
                 {
                     "name": "7/11 Poblacion Pandi (End)",
-                    "fare": "₱25.00",
+                    "estimated fare": "₱25.00",
                     "coords": [120.95757576729208, 14.865011620788547]
                 }
             ]
@@ -64,22 +64,22 @@ const mapData = {
             "stops": [
                 {
                     "name": "DiviMart Bunsuran 1st Pandi",
-                    "fare": "₱15.00",
+                    "estimated fare": "₱15.00",
                     "coords": [120.93978879612719, 14.862987113288161]
                 },
                 {
                     "name": "Bunsuran National High School",
-                    "fare": "₱18.00",
+                    "estimated fare": "₱18.00",
                     "coords": [120.93507605195134, 14.863987272712112]
                 },
                 {
                     "name": "LBC Express - Bunsuran 3rd",
-                    "fare": "₱22.00",
+                    "estimated fare": "₱22.00",
                     "coords": [120.92920028263276, 14.861635684467535]
                 },
                 {
                     "name": "Chowking Balagtas (End)",
-                    "fare": "₱26.00",
+                    "estimated fare": "₱26.00",
                     "coords": [120.90620553845629, 14.818098038137249]
                 }
             ]
@@ -92,22 +92,22 @@ const mapData = {
             "stops": [
                 {
                     "name": "Ad Santiago's Oil Station",
-                    "fare": "₱15.00",
+                    "estimated fare": "₱15.00",
                     "coords": [120.8821905961268, 14.821653991688327]
                 },
                 {
                     "name": "BigBrew Santol Balagtas Bulacan",
-                    "fare": "₱20.00",
+                    "estimated fare": "₱20.00",
                     "coords": [120.91723488263267, 14.85449093191257]
                 },
                 {
                     "name": "Divimart Mapulang Lupa Pandi",
-                    "fare": "₱24.00",
+                    "estimated fare": "₱24.00",
                     "coords": [120.96661874030346, 14.887459359618925]
                 },
                 {
                     "name": "Cacarong Matanda Jeepney Terminal (End)",
-                    "fare": "₱28.00",
+                    "estimated fare": "₱28.00",
                     "coords": [120.96784512496296, 14.907323801744086]
                 }
             ]
@@ -120,22 +120,22 @@ const mapData = {
             "stops": [
                 {
                     "name": "Amana Waterpark",
-                    "fare": "₱16.00",
+                    "estimated fare": "₱16.00",
                     "coords": [120.95235743845717, 14.890257914083605]
                 },
                 {
                     "name": "LBC Express - Bunsuran 3rd",
-                    "fare": "₱20.00",
+                    "estimated fare": "₱20.00",
                     "coords": [120.92916809612728, 14.86155272496997]
                 },
                 {
                     "name": "Ad Santiago's Oil Station",
-                    "fare": "₱24.00",
+                    "estimated fare": "₱24.00",
                     "coords": [120.90534308078637, 14.848128444486202]
                 },
                 {
                     "name": "Chowking Balagtas (End)",
-                    "fare": "₱28.00",
+                    "estimated fare": "₱28.00",
                     "coords": [120.90617335195078, 14.818066922065313]
                 }
             ]
@@ -148,22 +148,22 @@ const mapData = {
             "stops": [
                 {
                     "name": "Alfamart Real De Cacarong",
-                    "fare": "₱16.00",
+                    "estimated fare": "₱16.00",
                     "coords": [120.97920945379823, 14.9184156576699]
                 },
                 {
                     "name": "Alfamart Niugan Angat",
-                    "fare": "₱20.00",
+                    "estimated fare": "₱20.00",
                     "coords": [120.9713362588314, 14.9456509747636]
                 },
                 {
                     "name": "36 C.L. Hilario St, Bustos, 3007 Bulacan Petron",
-                    "fare": "₱25.00",
+                    "estimated fare": "₱25.00",
                     "coords": [120.92050099428214, 14.953954313028458]
                 },
                 {
                     "name": "Jollibee Baliwag Junction (End)",
-                    "fare": "₱30.00",
+                    "estimated fare": "₱30.00",
                     "coords": [120.89718228078775, 14.963435286267357]
                 }
             ]
@@ -176,22 +176,22 @@ const mapData = {
             "stops": [
                 {
                     "name": "Meralco Plaridel Sector",
-                    "fare": "₱13.00",
+                    "estimated fare": "₱13.00",
                     "coords": [120.84875703739117, 14.885296932227957]
                 },
                 {
                     "name": "The Galilee Memorial Park",
-                    "fare": "₱16.00",
+                    "estimated fare": "₱16.00",
                     "coords": [120.82115113724038, 14.869445049459658]
                 },
                 {
                     "name": "Hiyas ng Bulacan Convention Center",
-                    "fare": "₱19.00",
+                    "estimated fare": "₱19.00",
                     "coords": [120.81576923466476, 14.856983070715152]
                 },
                 {
                     "name": "Bulacan Provincial Capitol (End)",
-                    "fare": "₱22.00",
+                    "estimated fare": "₱22.00",
                     "coords": [120.8143724485936, 14.856531968336274]
                 }
             ]
@@ -204,72 +204,72 @@ const mapData = {
             "stops": [
                 {
                     "name": "Primark Center Plaridel",
-                    "fare": "₱18.00",
+                    "estimated fare": "₱18.00",
                     "coords": [120.86712014452647, 14.89137447965088]
                 },
                 {
                     "name": "588 Shopping Mall",
-                    "fare": "₱22.00",
+                    "estimated fare": "₱22.00",
                     "coords": [120.86754163915788, 14.90074418098115]
                 },
                 {
                     "name": "Baliwag-Pulilan Boundary",
-                    "fare": "₱25.00",
+                    "estimated fare": "₱25.00",
                     "coords": [120.8732210831171, 14.918402608357866]
                 },
                 {
                     "name": "New Pag-Ibig Fund Baliuag Branch",
-                    "fare": "₱30.00",
+                    "estimated fare": "₱30.00",
                     "coords": [120.8868609378562, 14.955680955615621]
                 },
                 {
                     "name": "Waterwood Park",
-                    "fare": "₱33.00",
+                    "estimated fare": "₱33.00",
                     "coords": [120.88849685707129, 14.957838457758697]
                 },
                 {
                     "name": "SM City Baliuag",
-                    "fare": "₱36.00",
+                    "estimated fare": "₱36.00",
                     "coords": [120.89032103694966, 14.960240534937066]
                 },
                 {
                     "name": "Jollibee Baliwag Junction",
-                    "fare": "₱40.00",
+                    "estimated fare": "₱40.00",
                     "coords": [120.89726071051938, 14.96332619254229]
                 },
                 {
                     "name": "Waltermart Baliwag",
-                    "fare": "₱45.00",
+                    "estimated fare": "₱45.00",
                     "coords": [120.90340189953895, 14.966756739373961]
                 },
                 {
                     "name": "San Rafael New Municipal Hall",
-                    "fare": "₱50.00",
+                    "estimated fare": "₱50.00",
                     "coords": [120.92770049443799, 14.982974266593242]
                 },
                 {
                     "name": "Capihan Elementary School",
-                    "fare": "₱55.00",
+                    "estimated fare": "₱55.00",
                     "coords": [120.93039746954943, 14.997638703004343]
                 },
                 {
                     "name": "San Rafael Public Market",
-                    "fare": "₱60.00",
+                    "estimated fare": "₱60.00",
                     "coords": [120.93370825864571, 15.025440786890847]
                 },
                 {
-                    "name": "Savemor Mareket San Ildelfonso",
-                    "fare": "₱65.00",
+                    "name": "Savemore Mareket San Ildelfonso",
+                    "estimated fare": "₱65.00",
                     "coords": [120.94129553483234, 15.079379875562154]
                 },
                 {
                     "name": "Meralco San Ildelfonso Substation",
-                    "fare": "₱70.00",
+                    "estimated fare": "₱70.00",
                     "coords": [120.9408871691267, 15.103277430062896]
                 },
                 {
                     "name": "San Ildelfonso New Municipal Hall (End)",
-                    "fare": "₱75.00",
+                    "estimated fare": "₱75.00",
                     "coords": [120.94146977666458, 15.105046444665886]
                 }
             ]
